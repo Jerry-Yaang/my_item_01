6c483dce6e912b9c750be143ffb184366117e577
Jerry-Yaang <1372700140@qq.com>
2024-09-13 02:17:32 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './styles/base.css' // css 样式重置
+import './styles/common.css' // 公共全局样式
+import './assets/iconfont/iconfont.css' // 字体图标的样式
+import NewGoodItem from './components/NewGoodItem.vue'
 Vue.config.productionTip = false
-
+Vue.component('NewGoodItem', NewGoodItem)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
